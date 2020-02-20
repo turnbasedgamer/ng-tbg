e961c5303a8aca16cb738f3e096acb9c74e5d5e5
turnbasedgamer <turnbasedgamer@protonmail.com>
2020-02-20 09:26:24 +0000

    LOgic stuff

--- src/common/logic/LogicSystem.ts
+++ src/common/logic/LogicSystem.ts
@@ -1,27 +1,22 @@
+import functions from './Functions.json';
+
 export class LogicSystem {
   public call(functionName: string, args: any[]) {
-    let f = this.resolveFunction(functionName);
+    let f: any = this.resolve(functionName);
     f(...args);
   }
 
-  private resolveFunction(functionName: string) {
-
-  }
-}
-
-export class Functions {
-  public getFunction(name: string) {
-    return this.functions.find((f) => f.name === name)
+  public resolve(name: string): any  {
+    return this.functions.find((f) => f.name === name).func;
   }
 
   private functions: Array<Function> = [
-    {
-      name: "TestFunction",
-      func: (arg1: any, arg2: any) => {
-        console.log(`Resolving function Test, with args ${arg1}, ${arg2}`);
-      }
-    }
+    
   ];
+}
+
+export class Functions {
+  
 }
 
 class Function {
